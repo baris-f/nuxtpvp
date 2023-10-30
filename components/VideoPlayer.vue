bcbf8344cf434db0e070928678649161f54fd92a
baris-f <florent.baris@epitech.eu>
2023-10-30 17:49:18 +0900

    Switch to nuxt Course to publish on netlify

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup></script>
+
+<template>
+  <iframe
+    width="460"
+    height="270s"
+    :src="`https://player.vimeo.com/video/${props.videoId}`"
+    title="Video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>
+</template>
+
+
+<script setup lang="ts">
+const props = defineProps({
+  videoId: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped></style>
